5a9dd1e5b572bd5dfdcca9781d45fecb7d17b347
Thuy Nguyen <thuicide@gmail.com>
2022-10-10 07:53:28 -0700

    Assignment 7-2

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-
+# Get values and store in both lists
 num1 = list(map(int, input().split()))
 num2 = list(map(int, input().split()))
 
-# ******************************
-# Make your Code
-# ******************************
+# Compare strings to see if num2 is a substring of num1
+i = 0
+j = 0
+isSubstring = False
+for i in range(len(num2)):
+    for j in range(len(num1)):
+        if num2[i] == num1[j]:
+            isSubstring = True
 
-# print ('True') or print ('False')
+# Print true or false
+if isSubstring == True:
+    print("True")
+else:
+    print("False")
